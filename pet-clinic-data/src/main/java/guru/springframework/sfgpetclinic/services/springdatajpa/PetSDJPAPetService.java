72d621479e25e44dfbdc4b11d7a103e4e50ed776
Tim Johnson <johnsont53@gmail.com>
2021-06-17 18:16:21 -0400

    Added Specialty JPA service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJPAPetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJPAPetService.java
@@ -1,13 +1,11 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.services.PetService;
