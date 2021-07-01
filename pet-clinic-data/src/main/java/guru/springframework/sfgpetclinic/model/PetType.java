118b8e153382bbedfe843f9d9ffc631b83e7ddc5
Tim Johnson <johnsont53@gmail.com>
2021-07-01 10:24:06 -0400

    Added some pet functionality. #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -20,6 +20,12 @@ import lombok.Setter;
 @Table(name = "types")
 public class PetType extends BaseEntity {
 	
+	@Builder
+	public PetType(Long id, String name) {
+	    super(id);
+	    this.name = name;
+	}
+	
 	@Column(name = "name")
 	private String name;
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
