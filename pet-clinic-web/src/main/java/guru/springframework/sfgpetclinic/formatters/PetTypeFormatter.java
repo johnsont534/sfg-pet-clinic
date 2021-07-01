1082788df1475f1390e41351c6e64c6f84d133c3
Tim Johnson <johnsont53@gmail.com>
2021-07-01 14:12:16 -0400

    Fixed PetType parsing for options. Closes #60

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/formatters/PetTypeFormatter.java
@@ -0,0 +1,42 @@
+package guru.springframework.sfgpetclinic.formatters;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Set;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+	private final PetTypeService petTypeService;
+	
+	public PetTypeFormatter(PetTypeService petTypeService) {
+		super();
+		this.petTypeService = petTypeService;
+	}
+
+	@Override
+	public String print(PetType petType, Locale locale) {
+		return petType.getName();
+	}
+
+	@Override
+	public PetType parse(String text, Locale locale) throws ParseException {
+		
+		 Set<PetType> findPetTypes = petTypeService.findAll();
+
+	     for (PetType type : findPetTypes) {
+	         if (type.getName().equals(text)) {
+	             return type;
+	         }
+	     }
+
+	     throw new ParseException("type not found: " + text, 0);
+	}
+
+}
